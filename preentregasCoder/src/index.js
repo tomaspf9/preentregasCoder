046095937e3e103b393504ae7901f9f4a5e99759
tomaspf9 <84751753+tomaspf9@users.noreply.github.com>
2023-06-15 18:42:31 -0300

    Add files via upload

--- preentregasCoder/src/index.js
+++ preentregasCoder/src/index.js
@@ -1,6 +1,21 @@
 const express = require ('express')
 const app = express ()
 const port = 8080
-import products from ""
-    
+import products from "./routes/products.router.js"
+const http = require('http')
+
+
+app.get('/', (req, res) =>{
+    res.send('hello API !')
+})
+const server = http.createServer((req, res) =>{
+    res.writeHead(200,{
+        'Content-Type': 'text/plain',
+    })
+    res.end('Hello!')
+})
+
+server.listen(port, () =>{
+    console.log(`listening on port ${port}...`)
+})
     
