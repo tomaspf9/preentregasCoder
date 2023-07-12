1ea7644d7656efe6b10fa55d021a80b641c897d4
tomaspf9 <84751753+tomaspf9@users.noreply.github.com>
2023-07-12 17:21:32 -0300

    Add files via upload

---
+++ Integradora/src/dao/mongo/models/cart.model.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const cartCollection = "carts";
+
+const cartSchema = new mongoose.Schema({
+  products: Array,
+});
+
+export const cartModel = mongoose.model(cartCollection, cartSchema);
