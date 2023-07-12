bf0c43c24fd9585ae11457bf17596ff2c939e1ca
tomaspf9 <84751753+tomaspf9@users.noreply.github.com>
2023-07-12 17:19:31 -0300

    Update app.js

--- Integradora/src/app.js
+++ Integradora/src/app.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import mongoose from 'mongoose';
+import productRouter from "./routes/product.routes"
+import chatRouter from "./routes/chat.routes"
+import cartRouter from "./routes/cart.routes"
 
 import __dirname from "./utils.js";
+import { Server } from 'socket.io';
 
 const app = express();
 const port = 8080;
-const mongoURL = await mongoose.connect(
-    "mongodb+srv://tomaspf33:coder@cluster0.hwqwlmp.mongodb.net/?retryWrites=true&w=majority");
 
-app.engine("handlebars", engine());
+mongoose.connect(
+    "mongodb+srv://tomaspf33:coder@cluster0.hwqwlmp.mongodb.net/?retryWrites=true&w=majority")
+
+app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
-app.set("views", "./src/views");
+app.set("views", __dirname + "/views");
 app.use(express.static("public"));
 
 const server = app.listen(port, () => {
@@ -68,5 +73,5 @@ io.on("connection", (socket) => {
 
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
-app.use("/api/viewProducts", viewsRouter);
+//app.use("/api/viewProducts", viewsRouter);
 app.use("/chat", chatRouter);
