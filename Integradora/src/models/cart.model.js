24dd72931889291c6708cf10a4f09131a0acd5ab
tomaspf9 <84751753+tomaspf9@users.noreply.github.com>
2023-07-12 17:22:05 -0300

    Delete Integradora/src/models directory

--- Integradora/src/models/cart.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const cartCollection = "carts";
-
-const cartSchema = new mongoose.Schema({
-  products: Array,
-});
-
-export const cartModel = mongoose.model(cartCollection, cartSchema);
