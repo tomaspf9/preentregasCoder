4389747cc3f0d794bf3a87804131da6b440a722f
tomaspf9 <84751753+tomaspf9@users.noreply.github.com>
2023-07-13 20:38:05 -0300

    Update cart.model.js

--- Integradora/src/dao/mongo/models/cart.model.js
+++ Integradora/src/dao/mongo/models/cart.model.js
@@ -3,7 +3,22 @@ import mongoose from "mongoose";
 const cartCollection = "carts";
 
 const cartSchema = new mongoose.Schema({
-  products: Array,
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "products",
+      },
+      quantity: {
+        type: String,
+        default: 1,
+      },
+    },
+  ],
+});
+
+cartSchema.pre("find", function () {
+  this.populate("products.product");
 });
 
 export const cartModel = mongoose.model(cartCollection, cartSchema);
