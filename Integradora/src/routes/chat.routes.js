e1be991aff78f3de8d6035fe44ffe7f5f8e45f14
tomaspf9 <84751753+tomaspf9@users.noreply.github.com>
2023-07-13 20:27:41 -0300

    Update chat.routes.js

--- Integradora/src/routes/chat.routes.js
+++ Integradora/src/routes/chat.routes.js
@@ -3,24 +3,23 @@ import { chatModel } from "../models/chat.model.js";
 
 const router = Router();
 
-router.get('/', async (req, res) => {
-    res.render('chat', {} )
-})
+router.get("/", async (req, res) => {
+  res.render("chat", {});
+});
 
-router.get('/messages', async (req, res) => {
-    try{
-        const messages = await chatModel.find()
-        res.send(messages)
-    }
-    catch(err){
-        req.statusCode(500).send(err.message)
-    }
-})
+router.get("/messages", async (req, res) => {
+  try {
+    const messages = await chatModel.find();
+    res.send(messages);
+  } catch (err) {
+    req.statusCode(500).send(err.message);
+  }
+});
 
-router.post('/', async (req, res) => {
-    const message = req.body
-    const response = await chatModel.create(message)
-    res.send(response)
-})
+router.post("/", async (req, res) => {
+  const message = req.body;
+  const response = await chatModel.create(message);
+  res.send(response);
+});
 
 export default router;
