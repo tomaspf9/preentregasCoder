c9fa38d46b17be122613c976bcc76ac3d77ebcd9
tomaspf9 <84751753+tomaspf9@users.noreply.github.com>
2023-07-20 16:27:13 -0300

    Update views.routes.js

--- segundaPreentrega/src/routes/views.routes.js
+++ segundaPreentrega/src/routes/views.routes.js
@@ -1,13 +1,32 @@
-import { Router } from "express";
+import {Router} from 'express'
 
-const router = Router();
+const views = Router()
 
-router.get('/', (req,res)=>{
-    res.render('products',{})
+const estaLogueado = (req, res, next) => {
+  if (req.session.user) {
+    return res.redirect('/profile');
+  }
+
+  next()
+}
+
+views.get('/login', estaLogueado, (req,res) => {
+  res.render('login')
 })
 
-router.get('/cart', (req,res)=>{
-    res.render('carts', {})
+views.get('/', (req,res) => {
+  res.render('index')
 })
 
-export default router
+
+views.get('/register', estaLogueado, (req,res) => {
+  res.render('register')
+})
+
+views.get('/profile', (req,res) => {
+  if (!req.session.user) return res.redirect('/login') 
+  res.render('profile', {user:req.session.user})
+})
+
+
+export default router;
